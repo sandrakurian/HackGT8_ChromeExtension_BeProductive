47091cb2456cb4adbc4f4011edb3a67a12c48c66
Sandra Kurian <34875437+sandrakurian@users.noreply.github.com>
2021-10-24 07:42:38 -0400

    clean up files

--- timer.js
+++ timer.js
@@ -8,15 +8,62 @@ const SEC = 0;
 
 var hr = HR;
 var min = MIN;
-var sec = SEC;
-var stoptime = true;
-
+document.getElementById("startTimer").addEventListener("click", startTimer);
 function startTimer() {
   if (stoptime == true) {
     stoptime = false;
-    timerCycle();
+    // timerCycle();
+    
+  }
+
+  if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+    hr = parseInt(hr);
+    
+    if (hr == 0 && min == 0 && sec == 0) {
+      stopTimer();
+      return;
+    }
+
+    sec = sec - 1;
+
+    if (sec == -1) {
+      hr = hr;
+      min = min - 1;
+      sec = 59;
+      if (min == -1 && hr > 0) {
+        hr = hr - 1;
+        min = 59;
+        sec = 59;
+      }
+    }
+
+    // if (min == 0 && hr > 0) {
+    //   hr = hr - 1;
+    //   min = 60;
+    //   sec = 0;
+    // }
+    
+
+    if (sec < 10 || sec == 0) {
+      sec = '0' + sec;
+    }
+    if (min < 10 || min == 0) {
+      min = '0' + min;
+    }
+    if (hr < 10 || hr == 0) {
+      hr = '0' + hr;
+    }
+
+    timer.innerHTML = hr + ':' + min + ':' + sec;
+
+    setTimeout("startTimer()", 1000);
+    // setTimeout("timerCycle()", 250); // testing
   }
 }
+
+document.getElementById("stopTimer").addEventListener("click", stopTimer);
 function stopTimer() {
   if (stoptime == false) {
     stoptime = true;
@@ -72,6 +119,7 @@ function timerCycle() {
   }
 }
 
+document.getElementById("resetTimer").addEventListener("click", resetTimer);
 function resetTimer() {
   hr = HR;
   min = MIN;
